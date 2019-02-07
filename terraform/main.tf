6789e39672eee42fbe9dd18f2c778c9aedd16dc0
Daniel Ayo <daniel@danielayo.com>
2019-02-07 00:37:26 +0000

    added vars for local terraform use

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,11 @@
 provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   version = "=1.21.0"
+  client_id = "${var.client_id}"
+  client_secret = "${var.client_secret}"
+  subscription_id = "${var.subscription_id}"
+  tenant_id = "${var.tenant_id}"
+
 }
 
 resource "azurerm_resource_group" "aci-rg" {
